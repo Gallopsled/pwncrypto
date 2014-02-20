fb9eb15f72c92fbedaad799e2033b30cafccf7d8
Martin Bjerregaard Jepsen <martin@jepsen.no>
2014-02-20 20:14:41 +0100

    Cleaned up old files

--- math/__init__.py
+++
@@ -1,4 +0,0 @@
-'''Mathematical building blocks used for basic cryptographic theory and
-operations.'''
-
-from foreign import *
--- util.py
+++
@@ -1,5 +0,0 @@
-from Crypto.Util import number
-
-# String conversion
-s2n = number.bytes_to_long
-n2s = number.long_to_bytes
