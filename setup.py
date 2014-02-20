904091a182ced2a8ce82f527935dd8154cabe074
Kristoffer Søholm <k.soeholm@gmail.com>
2014-02-20 20:14:19 +0100

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='pwncrypto',
+    version='0.0.1',
+    author='pwnies',
+    author_email='pwnies@dikumail.dk',
+    packages=['pwncrypto'],
+    scripts=[],
+    url='http://github.com/pwnies/pwncrypto',
+    license='LICENSE.txt',
+    description='A python library implementing crypto attacks and utility functions',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "gmpy2 >= 2.0.3",
+        "PyCrypto >= 2.6.1"
+    ],
+)
